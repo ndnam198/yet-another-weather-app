be977e8bd7d4c43df874a9890c78e403e9976803
ndnam198 <ndnam198@gmail.com>
2023-05-10 15:15:14 +0700

    add: ui for weather screen

--- lib/weather/presentation/current_weather_page.dart
+++ lib/weather/presentation/current_weather_page.dart
@@ -1,9 +1,11 @@
+import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
+import '../../widgets/app_icons_icons.dart';
 import '../domain/current_weather_model.dart';
 import '../providers.dart';
+import 'widgets/weather_item.dart';
 
 class CurrentWeatherPage extends ConsumerStatefulWidget {
   const CurrentWeatherPage({super.key});
@@ -14,39 +16,65 @@ class CurrentWeatherPage extends ConsumerStatefulWidget {
 
 class _WeatherGeneralState extends ConsumerState<CurrentWeatherPage> {
   @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) =>
-        ref.read(currentWeatherControllerProvider.notifier).getWeather());
-  }
-
-  @override
   Widget build(BuildContext context) {
     AsyncValue<CurrentWeatherModel> state =
         ref.watch(currentWeatherControllerProvider);
 
-    // FIXME: UI layer. The task is to change this to a more appropriate widget.
     return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            state.when(
-              data: (item) => Text(item.temperature?.toString() ?? ""),
-              loading: () => const CircularProgressIndicator(),
-              error: (e, st) => Center(child: Text(e.toString())),
+      extendBody: true,
+      bottomNavigationBar: FloatingNavbar(
+        margin: const EdgeInsets.all(16),
+        onTap: (int id) {},
+        currentIndex: 0,
+        items: [
+          FloatingNavbarItem(icon: AppIcons.iconoir_home_simple, title: 'Home'),
+          FloatingNavbarItem(
+              icon: AppIcons.noun_search_4511428_1, title: 'Explore'),
+          FloatingNavbarItem(
+              icon: AppIcons.bi_bookmark_dash_fill, title: 'Bookmarks'),
+          FloatingNavbarItem(
+              icon: AppIcons.fluent_person_16_filled, title: 'Personal'),
+        ],
+        selectedItemColor: const Color.fromRGBO(216, 97, 145, 1),
+        unselectedItemColor: const Color(0xFFC4C4C4),
+        backgroundColor: Colors.white,
+        borderRadius: 20,
+      ),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        shadowColor: Colors.transparent,
+        backgroundColor: Colors.transparent,
+        foregroundColor: const Color(0xFFFBFBFB),
+        leadingWidth: 56 + 16,
+        leading: Image.asset(
+          'assets/images/app_logo.png',
+          height: 56,
+          width: 56,
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 16),
+            child: Icon(
+              AppIcons.filter_icon,
+              size: 24,
             ),
-            OutlinedButton(
-              onPressed: () {
-                context.go('/details');
-              },
-              child: const Text('Weather Details'),
-            ),
-          ],
+          )
+        ],
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(231, 84, 129, 1),
+              Color.fromRGBO(146, 156, 222, 1),
+            ],
+            end: Alignment.bottomCenter,
+            begin: Alignment.topCenter,
+          ),
+        ),
+        child: const Center(
+          child: WeatherItem(),
         ),
       ),
     );
